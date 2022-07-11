ecc456f017ca15678855c69500d0142ad6d4a603
dnguyen49 <61990386+dnguyen49@users.noreply.github.com>
2022-07-10 23:48:14 -0500

    added test case

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,10 @@
 // Write your tests here!
+let polybius = require("../src/polybius.js").polybius;
+const expect = require("chai").expect;
+
+describe("polybius", () => {
+  it("should encode as described in the passing requirements in the previous lesson", () => {
+    const test1 = polybius("message");
+    expect(test1).to.equal("23513434112251");
+  });
+});
