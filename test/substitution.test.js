79f0969ad29f1cb1267a5d8eb52e89c726ad04bc
dnguyen49 <61990386+dnguyen49@users.noreply.github.com>
2022-07-10 23:48:35 -0500

    added test case

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,10 @@
 // Write your tests here!
+let substitution = require("../src/substitution.js").substitution;
+let expect = require("chai").expect;
+
+describe("substitution", () => {
+  it("should encode as described in the passing requirements in the previous lesson", () => {
+    const test1 = substitution("message", "plmoknijbuhvygctfxrdzeswaq");
+    expect(test1).to.equal("ykrrpik");
+  });
+});
