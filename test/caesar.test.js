e4f16d72d0ddf781179609212dee4b94baac4a75
dnguyen49 <61990386+dnguyen49@users.noreply.github.com>
2022-07-10 23:47:45 -0500

    added test case

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,10 @@
 // Write your tests here!
+let caesar = require("../src/caesar.js").caesar;
+const expect = require("chai").expect;
+
+describe("caesar", () => {
+  it("should encode a message by shifting the letters", () => {
+    const test1 = caesar("Zebra Magazine", 3);
+    expect(test1).to.equal("cheud pdjdclqh");
+  });
+});
